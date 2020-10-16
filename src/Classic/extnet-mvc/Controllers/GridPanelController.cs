7ba65aa804fef1b6d2a2be7dce5feb59bf85390e
Geoffrey McGill <geoff@object.net>
2020-10-16 12:00:31 -0600

    initial draft of extnet-mvc template

---
+++ src/Classic/extnet-mvc/Controllers/GridPanelController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using Ext.Net;
+using Ext.Net.Core;
+
+using Company.MvcApplication.Models;
+
+namespace Company.MvcApplication.Controllers
+{
+    public class GridPanelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new GridPanelModel());
+        }
+    }
+}
---
+++ src/Classic/extnet-mvc/Models/HomeModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Company.MvcApplication.Models
+{
+    public class HomeModel
+    {
+
+    }
+}
